4bba981d289b5c2e0ac42901bd1dffb03f9c38e1
Tim <timlabro@gmail.com>
2023-03-18 16:12:43 +0800

    Sensor to Serial Communcation to Firebase realtime db

--- Esp32/ParseInt_testing/ParseInt_testing.ino
+++ Esp32/ParseInt_testing/ParseInt_testing.ino
@@ -3,6 +3,34 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+//======================================== 
+
+//======================================== Insert your network credentials.
+#define WIFI_SSID "Labro"
+#define WIFI_PASSWORD "OedipusElectra1993"
+//======================================== 
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Defines the Digital Pin of the "On Board LED".
+#define On_Board_LED 2
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyBOHSeBCviQEJHEREcpAYDAXI36tciFJlE"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://esp32-testing-959e3-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
@@ -13,6 +41,23 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define RXp2 16
 #define TXp2 17
+
+// Define Firebase Data object.
+FirebaseData fbdo;
+
+// Define firebase authentication.
+FirebaseAuth auth;
+
+// Definee firebase configuration.
+FirebaseConfig config;
+
+//======================================== Millis variable to send/store data to firebase database.
+unsigned long sendDataPrevMillis = 0;
+const long sendDataIntervalMillis = 10000; //--> Sends/stores data to firebase database every 10 seconds.
+//======================================== 
+
+// Boolean variable for sign in status.
+bool signupOK = false;
 
 int test = 0;
 
@@ -25,11 +70,75 @@ void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
   display.clearDisplay();
+
+   pinMode(On_Board_LED, OUTPUT);
+
+  //---------------------------------------- The process of connecting the WiFi on the ESP32 to the WiFi Router/Hotspot.
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("---------------Connection");
+  Serial.print("Connecting to : ");
+  Serial.println(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+
+    digitalWrite(On_Board_LED, HIGH);
+    delay(250);
+    digitalWrite(On_Board_LED, LOW);
+    delay(250);
+  }
+  digitalWrite(On_Board_LED, LOW);
+  Serial.println();
+  Serial.print("Successfully connected to : ");
+  Serial.println(WIFI_SSID);
+  //Serial.print("IP : ");
+  //Serial.println(WiFi.localIP());
+  Serial.println("---------------");
+  //---------------------------------------- 
+
+  // Assign the api key (required).
+  config.api_key = API_KEY;
+
+  // Assign the RTDB URL (required).
+  config.database_url = DATABASE_URL;
+
+  // Sign up.
+  Serial.println();
+  Serial.println("---------------Sign up");
+  Serial.print("Sign up new user... ");
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  Serial.println("---------------");
+  
+  // Assign the callback function for the long running token generation task.
+  config.token_status_callback = tokenStatusCallback; //--> see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 void loop() {
-    Serial.println("Message Received: ");
+   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > sendDataIntervalMillis || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+
     while(Serial2.available()>0){
-      test = Serial2.parseInt();
+    test = Serial2.parseInt();
+    /*while(Serial2.available()>0){
+    }*/
+
+    Serial.println();
+    Serial.println("---------------Store Data");
+    digitalWrite(On_Board_LED, HIGH);
+
+    if (Firebase.RTDB.setFloat(&fbdo, "Test/Lux", test)) {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+
       Serial.println(test);
       display.setTextSize(1);
       display.setTextColor(WHITE);
@@ -37,7 +146,15 @@ void loop() {
       display.println(test);
       display.display();
       display.clearDisplay();
-      delay(1000);
     }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    digitalWrite(On_Board_LED, LOW);
+    Serial.println("---------------");
     //Serial.println(Serial2.readString());
+    }
+  }
 }
