c43a048e07750643ee491feb1faed25a813e38aa
Tim <timlabro@gmail.com>
2023-03-23 12:45:20 +0800

    DHT11 (Temp and Humidity sensor) and LTR390 (UV sensor) test

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,18 +1,74 @@
-#include <BH1750.h>
+#include <dht.h>
+#include "Adafruit_LTR390.h"
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-BH1750 lightMeter;
+#define DHT11_PIN 7
 
-void setup() {
-  Serial.begin(9600);
+dht DHT;
+Adafruit_LTR390 ltr = Adafruit_LTR390();
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup(){
+  Serial.begin(115200);
   Wire.begin();
-  lightMeter.begin();
-  //Serial.println(F("BH1750 Test Begin"));
+
+  lcd.init();
+  lcd.backlight();
+
+  if ( ! ltr.begin() ) {
+    Serial.println("Couldn't find LTR sensor!");
+    while (1) delay(10);
+  }
+  Serial.println("Found LTR sensor!");
+
+  ltr.setMode(LTR390_MODE_UVS);
+  if (ltr.getMode() == LTR390_MODE_ALS) {
+    Serial.println("In ALS mode");
+  } else {
+    Serial.println("In UVS mode");
+  }
+
+  ltr.setGain(LTR390_GAIN_3);
+  Serial.print("Gain : ");
+  switch (ltr.getGain()) {
+    case LTR390_GAIN_1: Serial.println(1); break;
+    case LTR390_GAIN_3: Serial.println(3); break;
+    case LTR390_GAIN_6: Serial.println(6); break;
+    case LTR390_GAIN_9: Serial.println(9); break;
+    case LTR390_GAIN_18: Serial.println(18); break;
+  }
+
+  ltr.setResolution(LTR390_RESOLUTION_16BIT);
+  Serial.print("Resolution : ");
+  switch (ltr.getResolution()) {
+    case LTR390_RESOLUTION_13BIT: Serial.println(13); break;
+    case LTR390_RESOLUTION_16BIT: Serial.println(16); break;
+    case LTR390_RESOLUTION_17BIT: Serial.println(17); break;
+    case LTR390_RESOLUTION_18BIT: Serial.println(18); break;
+    case LTR390_RESOLUTION_19BIT: Serial.println(19); break;
+    case LTR390_RESOLUTION_20BIT: Serial.println(20); break;
+  }
+
+  ltr.setThresholds(100, 1000);
+  ltr.configInterrupt(true, LTR390_MODE_UVS);
 }
-void loop() {
-  float lux = lightMeter.readLightLevel();
-  Serial.print("Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
-  delay(1000);
+
+void loop(){
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+
+  if (ltr.newDataAvailable()) {
+    Serial.print("UV data: "); 
+    Serial.println(ltr.readUVS());
+    lcd.setCursor(0, 0);
+    lcd.print("UV : ");
+    lcd.print(ltr.readUVS());
+  }
+
+  delay(1100);
 }
